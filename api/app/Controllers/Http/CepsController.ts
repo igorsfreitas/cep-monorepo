a19faaf95206f08f3af6e932bb25559d5f4cd4d9
Igor Freitas <igorfreitas1992@gmail.com>
2021-09-23 15:20:11 -0300

    Added swagger to project

--- api/app/Controllers/Http/CepsController.ts
+++ api/app/Controllers/Http/CepsController.ts
@@ -3,6 +3,34 @@ import { getAddressByCep } from 'App/Services/ViaCep'
 import { schema } from 'App/Validation/CepValidator'
 
 export default class CepsController {
+
+  /**
+  * @swagger
+  * /address:
+  *   get:
+  *     tags:
+  *       - Address, CEP
+  *     summary: Get Address By CEP API
+  *     parameters:
+  *       - name: cep
+  *         description: CEP of the required Address
+  *         in: query
+  *         required: true
+  *         type: string
+  *     responses:
+  *       200:
+  *         description: Send Address for this cep
+  *         example:
+  *           properties:
+  *            address:
+  *              type: object
+  *       400:
+  *         description: Throws validations errors or wrong cep error
+  *         example:
+  *           properties:
+  *            errors:
+  *              type: object
+  */
   public async getAddressByCep({request, response}: HttpContextContract) {
 
     const requestData = request.all()
